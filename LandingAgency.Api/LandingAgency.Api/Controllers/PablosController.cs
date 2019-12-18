45632eb939ef84420eaa91d3bb6e54b9f0caf0d1
Pablo Uribe Montoya <pablo.um00@gmail.com>
2019-12-18 11:41:21 -0500

    Pablo creating an api controller

---
+++ LandingAgency.Api/LandingAgency.Api/Controllers/PablosController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LandingAgency.Api.Controllers
+{
+    public class PablosController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Pablo", "Uribe" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "This is freaking unbelievable";
+        }
+    }
+}
